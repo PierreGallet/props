6f2fca117bf4176fc7b85598a0e5f32373a24103
Zack Fischmann <zacharyrobert@gmail.com>
2015-04-24 21:35:23 -0700

    change linkedin.js to app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var traverse = require('utils');
 var page = require('webpage').create();
 
+var urls = ['https://www.linkedin.com/in/frankbowers'];
+var username = 'zacharyrobert@gmail.com';
+var password = 'Buy me a burrito.';
+var skills = ['Software Engineering, Java, JavaScript, Node.js']
+
 page.open('http://www.linkedin.com', function() {
+  console.log('at linkedin');
   page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
     utils.logIn(username, password)
   }
