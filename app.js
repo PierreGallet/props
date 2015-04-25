71fb3cca9c7fa22f071cc18fbbcf50ce0278a11e
Zack Fischmann <zacharyrobert@gmail.com>
2015-04-24 21:43:36 -0700

    add test vars

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var traverse = require('utils');
 var page = require('webpage').create();
 
-var urls = ['https://www.linkedin.com/in/frankbowers'];
-var username = 'zacharyrobert@gmail.com';
-var password = 'Buy me a burrito.';
+var urls;
+var username;
+var password;
 var skills = ['Software Engineering, Java, JavaScript, Node.js']
+
+console.log(urls);
 
 page.open('http://www.linkedin.com', function() {
   console.log('at linkedin');
   page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
     utils.logIn(username, password)
+    console.log('logged in');
   }
   urls.forEach(function(url){
     page.open(url, function(){
+      console.log('opened url: ', url);
       page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
         page.evaluate(function() {
           utils.endorse(skills);
