8671848736da38402893f040f08a3e7a3ecc48f8
Zack Fischmann <zacharyrobert@gmail.com>
2015-04-25 18:22:48 -0700

    Update comments for easier readability

--- casperapp.js
+++ casperapp.js
@@ -1,35 +1,33 @@
 var cred = require('./cred.js');
 
+
+// DEFAULTS //
 var casper = require('casper').create({   
   verbose: true, 
   logLevel: 'debug',
   pageSettings: {
-    loadImages:  false,         // The WebPage instance used by Casper will
-    loadPlugins: false,         // use these settings
+    loadImages:  false,
+    loadPlugins: false,
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
   }
 });
 
-// print out all the messages in the headless browser context
+
+// PRINT OUT ALL MESSAGES IN HEADLESS BROWSER CONTEXT (for debugging) //
 casper.on('remote.message', function(msg) {
     this.echo('remote message caught: ' + msg);
 });
-
-// print out all the messages in the headless browser context
 casper.on("page.error", function(msg, trace) {
     this.echo("Page Error: " + msg, "ERROR");
 });
 
-//navigate to linkedin.com and log in
+// LOG IN LOGIC //
 casper.start('http://linkedin.com');
 casper.wait(1000, function(){
   casper.then(function() {
    this.echo("page loaded");
    this.test.assertExists('form#login', 'form is found');
    this.fill('form#login', { 
-        ////////////////////////////////////////////////
-        // Make sure to input username and pass below //
-        ////////////////////////////////////////////////
         'session_key': cred.username, 
         'session_password': cred.password
     }, true);
@@ -38,14 +36,13 @@ casper.wait(1000, function(){
 
 casper.wait(1000);
 
-//default skills and user url to endorse for testing
-var toEndorse = ['Java', 'JavaScript', 'Software Engineering'];
+// DEFAULT URL AND TO-ENDORSE SKILLS //
+var toEndorse = ['Java', 'JavaScript', 'Software Engineering', 'D3.js'];
 var userUrl = 'https://www.linkedin.com/in/frankbowers';
 
-//navigate to user url
+// NAVIGATE TO URL AND ENDORSE //
 casper.thenOpenAndEvaluate(userUrl, function(toEndorse, userUrl){
   var skills = $('.endorsable');
-  //if endorsables are in toEndorse list, click the endorse button
   for (var i=0; i<skills.length; i++){
     if(toEndorse.indexOf($(skills[i]).data('endorsedItemName')) > -1){
       $(skills[i]).find('.endorse-button')[0].click();
